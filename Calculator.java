1e3f51de65910214b5c3e405e31f7a0438f4de3a
omniedrx <drixomnhielope@gmail.com>
2026-08-16 21:26:23 +0800

    Added invalid number checker

--- Calculator.java
+++ Calculator.java
@@ -4,37 +4,42 @@ public class Calculator {
         Scanner scan = new Scanner(System.in);
 
         while (true) {
-            System.out.print("Enter first number: ");
-            double num1 = Double.valueOf(scan.nextLine());
-            System.out.print("Enter operator (+, -, *, /): ");
-            String operator = scan.nextLine();
-            System.out.print("Enter second number: ");
-            double num2 = Double.valueOf(scan.nextLine());
+            try {
+                System.out.print("Enter first number: ");
+                double num1 = Double.valueOf(scan.nextLine());
+                System.out.print("Enter operator (+, -, *, /): ");
+                String operator = scan.nextLine();
+                System.out.print("Enter second number: ");
+                double num2 = Double.valueOf(scan.nextLine());
 
-            double result = 0;
-            if (operator.equals("+")) {
-                result = num1 + num2;
-            } else if (operator.equals("-")) {
-                result = num1 - num2;
-            } else if (operator.equals("*")) {
-                result = num1 * num2;
-            } else if (operator.equals("/")) {
-                result = num1 / num2;
-            } else {
-                System.out.println("Invalid operator");
+                double result = 0;
+                if (operator.equals("+")) {
+                    result = num1 + num2;
+                } else if (operator.equals("-")) {
+                    result = num1 - num2;
+                } else if (operator.equals("*")) {
+                    result = num1 * num2;
+                } else if (operator.equals("/")) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Invalid operator");
+                    continue;
+                }
+
+                System.out.println();
+                System.out.println("Result: " + result);
+                System.out.println();
+                System.out.print("Calculate again? (y/n): ");
+                String repeat = scan.nextLine();
+
+                if (repeat.equals("y")) {
+                    continue;
+                }
+
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number. Please enter a number.");
             }
-
-            System.out.println();
-            System.out.println("Result: " + result);
-            System.out.println();
-            System.out.print("Calculate again? (y/n): ");
-            String repeat = scan.nextLine();
-
-            if (repeat.equals("y")) {
-                continue;
-            }
-
-            break;
         }
     }
 }
