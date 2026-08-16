85df84567a4e2377ac60a218ce4c28218a437c8e
omniedrx <drixomnhielope@gmail.com>
2026-08-16 21:14:06 +0800

    Added loop for the program

--- Calculator.java
+++ Calculator.java
@@ -3,26 +3,38 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        double num1 = Double.valueOf(scan.nextLine());
-        System.out.print("Enter operator (+, -, *, /): ");
-        String operator = scan.nextLine();
-        System.out.print("Enter second number: ");
-        double num2 = Double.valueOf(scan.nextLine());
+        while (true) {
+            System.out.print("Enter first number: ");
+            double num1 = Double.valueOf(scan.nextLine());
+            System.out.print("Enter operator (+, -, *, /): ");
+            String operator = scan.nextLine();
+            System.out.print("Enter second number: ");
+            double num2 = Double.valueOf(scan.nextLine());
 
-        double result = 0;
-        if (operator.equals("+")) {
-            result = num1 + num2;
-        } else if (operator.equals("-")) {
-            result = num1 - num2;
-        } else if (operator.equals("*")) {
-            result = num1 * num2;
-        } else if (operator.equals("/")) {
-            result = num1 / num2;
-        } else {
-            System.out.println("Invalid operator");
+            double result = 0;
+            if (operator.equals("+")) {
+                result = num1 + num2;
+            } else if (operator.equals("-")) {
+                result = num1 - num2;
+            } else if (operator.equals("*")) {
+                result = num1 * num2;
+            } else if (operator.equals("/")) {
+                result = num1 / num2;
+            } else {
+                System.out.println("Invalid operator");
+            }
+
+            System.out.println();
+            System.out.println("Result: " + result);
+            System.out.println();
+            System.out.print("Calculate again? (y/n): ");
+            String repeat = scan.nextLine();
+
+            if (repeat.equals("y")) {
+                continue;
+            }
+
+            break;
         }
-
-        System.out.println("Result: " + result);
     }
 }
