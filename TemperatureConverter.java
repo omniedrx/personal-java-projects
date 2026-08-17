f7036a0eb06f88de279ae2b9f504ff48515da3b1
omniedrx <drixomnhielope@gmail.com>
2026-08-17 14:33:04 +0800

    Added Kelvin conversion for both Celsius & Fahrenheit

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -9,6 +9,10 @@ public class TemperatureConverter {
         System.out.println("Choose the conversion:");
         System.out.println("1. Celsius to Fahrenheit");
         System.out.println("2. Fahrenheit to Celsius");
+        System.out.println("3. Celsius to Kelvin");
+        System.out.println("4. Kelvin to Celsius");
+        System.out.println("5. Fahrenheit to Kelvin");
+        System.out.println("6. Kelvin to Fahrenheit");
 
         System.out.println();
         System.out.print("Enter your choice: ");
@@ -24,8 +28,20 @@ public class TemperatureConverter {
         } else if (choice == 2) {
             converted = (temperature - 32) * 5/9;
             System.out.println(temperature + "°F = " + converted + "°C");
-        } else {
-            System.out.println("Invalid input");
+        } else if (choice == 3) {
+            converted = temperature + 273.15;
+            System.out.println(temperature + "°C = " + converted + " K");
+        } else if  (choice == 4) {
+            converted = temperature - 273.15;
+            System.out.println(temperature + " K = " + converted + "°C");
+        } else if (choice == 5) {
+            converted = (temperature - 32) * 5/9 + 273.15;
+            System.out.println(temperature + "°F = " + converted + " K");
+        } else if (choice == 6) {
+            converted = (temperature - 273.15) * 9/5 + 32;
+            System.out.println(temperature + " K= " + converted + "°F");
+        } else  {
+            System.out.println("Invalid number");
         }
     }
 }
